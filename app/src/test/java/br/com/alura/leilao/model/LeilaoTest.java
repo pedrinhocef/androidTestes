f117a84a3fd78664b652ca3b584eba72f982e01c
Pedro Soares <pedrinhocef@gmail.com>
2018-07-12 22:25:01 -0300

    Criando mais testes de unidade de forma descritiva

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -7,8 +7,7 @@ import static org.junit.Assert.*;
 public class LeilaoTest {
 
     @Test
-    public void getDescricao() {
-
+    public void getDescricaoQuandoRecebeDescricaoDevolveDescricao() {
         //Cenário de teste
 
         Leilao leilao = new Leilao("Console");
@@ -20,5 +19,57 @@ public class LeilaoTest {
         //Teste ação esperada
 
         assertEquals("Console", descricaoDevolvida);
+    }
+
+    @Test
+    public void getMaiorLanceQuandoRecebeUmLanceDevolveMaiorLance() {
+        //Cenário de teste
+        Leilao console = new Leilao("console");
+        console.propoe(new Lance(new Usuario("joao"), 500.00));
+
+        //Executar ação esperada
+        double maiorLanceEsperado = console.getMaiorLance();
+
+        double maiorEsperado = 500.00;
+        double delta = 0.0001;
+
+        //Teste ação esperada
+        assertEquals(maiorEsperado, maiorLanceEsperado, delta);
+    }
+
+    @Test
+    public void getMaiorLanceQuandoRecebeMaisDeUmLanceEmOrdemCrescenteDevolveMaiorLance() {
+        //Cenário de teste
+        Leilao console = new Leilao("console");
+        console.propoe(new Lance(new Usuario("joao"), 100.00));
+        console.propoe(new Lance(new Usuario("joao"), 500.00));
+
+        //Executar ação esperada
+        double maiorLanceEsperado = console.getMaiorLance();
+
+        double maiorEsperado = 500.00;
+        double delta = 0.0001;
+
+        //Teste ação esperada
+        assertEquals(maiorEsperado, maiorLanceEsperado, delta);
+
+    }
+
+    @Test
+    public void getMaiorLanceQuandoRecebeMaisDeUmLanceEmOrdemDecrescenteDevolveMaiorLAnce() {
+        //Cenário de teste
+        Leilao console = new Leilao("console");
+        console.propoe(new Lance(new Usuario("joao"), 800.00));
+        console.propoe(new Lance(new Usuario("joao"), 500.00));
+
+        //Executar ação esperada
+        double maiorLanceEsperado = console.getMaiorLance();
+
+        double maiorEsperado = 800.00;
+        double delta = 0.0001;
+
+        //Teste ação esperada
+        assertEquals(maiorEsperado, maiorLanceEsperado, delta);
+
     }
 }
